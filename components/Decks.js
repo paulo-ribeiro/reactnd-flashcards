1fcc311e77a6d7e65d436ab5cd2a2deadb4d37f9
paulo-ribeiro <paulo.ribeiro.dev@gmail.com>
2019-04-12 00:25:57 -0300

    feat: Added navigate to DeckDetail.

--- components/Decks.js
+++ components/Decks.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import Deck from './Deck';
 
-const Decks = ({ decks }) => (
+const Decks = ({ decks, ...props }) => (
   <View style={{ flex: 1 }}>
-    {Object.keys(decks).map(key => <Deck key={key} title={key} />)}
+    {Object.keys(decks).map(key =>
+      <TouchableOpacity key={key} onPress={() => props.navigation.navigate(
+        "DeckDetail",
+        { title: key })}>
+        <Deck title={key} />
+      </TouchableOpacity>)}
   </View>
 );
 
