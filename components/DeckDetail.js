38a6b328c593068f45725720c802b95893605a6c
paulo-ribeiro <paulo.ribeiro.dev@gmail.com>
2019-04-13 23:09:43 -0300

    feat: Added navigation to NewCard view and Quiz view. If there are no cards ErrorMsg is shown.

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -3,14 +3,42 @@ import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { grey, black, white } from '../utils/colors';
 import Button from './Button';
+import ErrorMsg from './ErrorMsg';
 
 class DeckDetail extends Component {
+  state = {
+    error: false
+  };
+
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.title
   });
 
+  onAddCard = () => {
+    const { navigation, title } = this.props;
+
+    this.setState({ error: false });
+
+    navigation.navigate(
+      "NewCard",
+      { title: title });
+  };
+
+  onStartQuiz = () => {
+    const { numOfCards, navigation, title } = this.props;
+
+    if (numOfCards === 0) {
+      this.setState({ error: true });
+      return;
+    }
+
+    navigation.navigate(
+      "Quiz",
+      { title: title });
+  };
+
   render() {
-    const { title, numOfCards, navigation } = this.props;
+    const { title, numOfCards } = this.props;
 
     return (
       <View style={{ flex: 1 }}>
@@ -19,19 +47,17 @@ class DeckDetail extends Component {
         <View style={styles.btnContainer}>
           <Button
             text={"Add Card"}
-            onPress={() => navigation.navigate(
-              "NewCard",
-              { title: title })}
+            onPress={this.onAddCard}
             btnStyle={{ backgroundColor: white, borderWidth: 1, borderColor: black }}
             txtStyle={{ color: black }} />
           <Button
             text={"Start Quiz"}
-            onPress={() => navigation.navigate(
-              "Quiz",
-              { title: title })}
+            onPress={this.onStartQuiz}
             btnStyle={{ backgroundColor: black, marginTop: 20 }}
             txtStyle={{ color: white }} />
           <Button />
+          {this.state.error
+            && <ErrorMsg text={"You dont have any cards \u{1F61E}"} txtStyle={{ textAlign: "center" }} />}
         </View>
       </View>
     );
