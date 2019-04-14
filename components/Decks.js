6c707c656e0a777686a21a6f9e51176f72933008
paulo-ribeiro <paulo.ribeiro.dev@gmail.com>
2019-04-14 13:33:58 -0300

    feat: Loading animation is shown while as decks are loaded from async store on mount.

--- components/Decks.js
+++ components/Decks.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
+import { AppLoading } from 'expo';
 import { connect } from 'react-redux';
+import { receiveDecks } from '../actions';
+import { fetchDecks } from '../utils/api';
 import Deck from './Deck';
 
-const Decks = ({ decks, navigation }) => (
-  <View style={{ flex: 1 }}>
-    {Object.keys(decks).map(key =>
-      <TouchableOpacity key={key} onPress={() => navigation.navigate(
-        "DeckDetail",
-        { title: key })}>
-        <Deck title={key} />
-      </TouchableOpacity>)}
-    {Object.keys(decks).length === 0
-      && <Text style={{ fontSize: 20, textAlign: "center", marginTop: 20 }}>
-        {"There are no decks added yet \u{1F61E}"}
-      </Text>}
-  </View>
-);
+class Decks extends Component {
+  state = {
+    ready: false
+  };
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    fetchDecks()
+      .then(decks => dispatch(receiveDecks(decks)))
+      .then(() => this.setState({ ready: true }));
+  }
+
+  render() {
+    if (!this.state.ready)
+      return <AppLoading />;
+
+    const { decks, navigation } = this.props;
+    return (
+      <View style={{ flex: 1 }}>
+        {Object.keys(decks).map(key =>
+          <TouchableOpacity key={key} onPress={() => navigation.navigate(
+            "DeckDetail",
+            { title: key })}>
+            <Deck title={key} />
+          </TouchableOpacity>)}
+        {Object.keys(decks).length === 0
+          && <Text style={{ fontSize: 20, textAlign: "center", marginTop: 20 }}>
+            {"There are no decks added yet \u{1F61E}"}
+          </Text>}
+      </View>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   decks: state
